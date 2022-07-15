e377572a73353678f8fe655ada413d3a267e1366
jindo takumi <92016292+jtakumi@users.noreply.github.com>
2022-07-15 19:51:41 +0900

    timeout event

---
+++ mopon.py
@@ -0,0 +1,35 @@
+import PySimpleGUI as psg
+import pyautogui as pag
+
+psg.theme('DarkBrown3')
+
+x,y=pag.position()
+xStr="X: " + str(x).rjust(4)
+yStr=" Y: " + str(y).rjust(4)
+
+layout=[[psg.Text('',key='-XP-',font=('',20))],
+        [psg.Text('',key='-YP-',font=('',20))],
+        [psg.Button('exit',size=(10,3))]
+]
+
+#window title
+wnd=psg.Window('mouse_pointer',layout,size=(300,200))
+
+#loop function
+while True:
+    x,y=pag.position()
+    xStr="X: " + str(x).rjust(4)
+    yStr="Y: " + str(y).rjust(4)
+    #reading window info
+    event,values=wnd.read(timeout=10,timeout_key='-timeout-')
+    #if click exit button
+    if event in (psg.WIN_CLOSED,'exit'):
+        break
+    #updating position
+    elif event == '-timeout-':
+        wnd['-XP-'].update(xStr)
+        wnd['-YP-'].update(yStr)
+    
+
+wnd.close()
+
